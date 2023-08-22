72b6c130164fe52c766ad316dbbdc74e8d805226
KangMinju <demo1208@gmail.com>
2023-08-22 11:49:41 +0900

    Read(1)기능구현

--- crud/urls.py
+++ crud/urls.py
@@ -21,5 +21,6 @@ from posts import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('index/', views.index),
+    path('posts/<int:id>', views.detail),
 ]
 
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
+# Post 모델을 import
+from .models import Post
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    posts = Post.objects.all()
+
+    context = {
+        'posts': posts,
+
+    }
+    return render(request, 'index.html', context)
+
+def detail(request, id):
+    post = Post.objects.get(id=id)
+
+    context = {
+        'post': post,
+    }
+    return render(request, 'detail.html', context )
+    
