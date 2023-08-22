8473600da6da60265a98c52bc82bd5b1534d0bf1
KangMinju <demo1208@gmail.com>
2023-08-22 14:33:27 +0900

    기능구현

--- posts/views.py
+++ posts/views.py
@@ -26,7 +26,8 @@ def new(request):
 def create(request):
     title = request.GET.get('title')
     content = request.GET.get('content')
-
+    
+    # 새로운 게시물을 만들 때 
     post = Post()
     post.title = title # 변수 할당 
     post.conten = content
@@ -39,3 +40,27 @@ def delete(request, id):
     post.delete()
 
     return redirect('/index/')
+def edit(request, id):
+    post = Post.objects.get(id=id)
+
+    context = {
+        'post': post,
+    }
+    # render는 html을 보여주기 위한 것 
+    return render(request,'edit.html', context)
+
+def update(request, id):
+    
+
+    # 방금 수정한 데이터
+    title = request.GET.get('title')
+    content = request.GET.get('content')
+
+    # post = Post() -> 새로운 게시물 만들 때 
+    # 기존데이터
+    post = Post.objects.get(id=id)
+    post.title = title
+    post.conten = content
+    post.save()
+
+    return redirect(f'/posts/{post.id}')
